52067a54c1b3a3cd99004cab1e9d4bc07eafdc13
valemdz <torresvalemdz@gmail.com>
2019-06-18 15:55:30 -0300

    base para una sola salidad y una sola entrada

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+import style from './style.css';
+import sass from './scss/main.scss';
 import { suma } from './suma';
+import  logo  from './img/react.png';
+
+import data from './datos.json';
+
+const d = document;
+var app = d.getElementById('app');
+var h1 = d.createElement('h1');
+var img = d.createElement('img');
+img.src = logo;
+h1.textContent = 'Trabajando con WebPack';
+
+app.appendChild(h1);
+app.appendChild(img);
 
 const profesor = 'VALERIA';
 console.log('Ejemplo de webpack sin configuracion');
 console.log(`Hola ${profesor}`);
+console.log( data );
 
 console.log(suma(9, 9));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
-const HtmlWebpackPlugin= require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractyPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports={
+    devtool:'source-map',
     module:{
         rules:[
             {
@@ -16,13 +20,52 @@ module.exports={
                     loader:'html-loader',
                     options:{minimize:true}                
                 }]
+            },
+            {
+                test:/\.(css|scss)/,
+                use:[
+                    'style-loader',
+                     MiniCssExtractyPlugin.loader,
+                     'css-loader',
+                     { 
+                         loader: 'postcss-loader',
+                         options:{
+                             autoprefixer:{
+                                 browser:['last 2 versions']
+                             },
+                             sourceMap:true,
+                             plugins: ()=>[autoprefixer]
+                         }
+                     },
+                     'resolve-url-loader',
+                     'sass-loader?outputStyle=compressed&sourceMap'
+                ]
+            },
+            {
+                test:/\.(jpe?g|png|gif|svg|webp)$/i,
+                use:[
+                    'file-loader?name=assets/[name].[ext]',
+                    'image-webpack-loader?bypassOnDegub'
+                ]
+            },
+            {
+                test:/\.(ttf|eoy|woff2?|mp4|mp3|xml|pdf)$/i,
+                use:'file-loader?name=assets/[name].[ext]'
             }
         ]
     },
     plugins:[
+        new CleanWebpackPlugin( ['dist/**/*.*'] ),
+        new MiniCssExtractyPlugin(),
         new HtmlWebpackPlugin({
             template:'./src/template.html',
             file:'index.html'
         })
     ]
 }
+
+
+// Revisar este archivo de conf que no lo levanta con estas opciones
+//'css-loader?minimize&sourceMap',
+
+//'dist/**/*.*' cualquier nivel interno de carpetas y cualquier tipo de archivos
